fe6bee718c4373201b4528687523298dd9fae1f5
keitaro <keitaro.yoda@outlook.com>
2018-06-02 18:22:02 +0900

    test

---
+++ src/application/Tetris_Obj.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Random;
+
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class Tetris_Obj {
+
+	// このメソッドが1秒間に60回ぐらい呼ばれるので
+	// テトリスの内部処理をここに書く
+	public void update() {
+
+		// ENTER押下
+		if (GameLib.isKeyOn("ENTER")) {
+
+		}
+
+		// P押下
+		if (GameLib.isKeyOn("P")) {
+
+			
+		}
+
+		// O押下
+		if (GameLib.isKeyOn("O")) {
+
+		}
+
+
+	}
+}
